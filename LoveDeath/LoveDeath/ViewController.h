9f4094d5a7b67ab58fa6ee0851ea887852fda035
陈芙蓉 <1303668144@qq.com>
2016-11-23 17:58:42 +0800

    初始化LoveDeath

---
+++ LoveDeath/LoveDeath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveDeath
+//
+//  Created by 曹欣宗 on 2016/11/23.
+//  Copyright © 2016年 xhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
